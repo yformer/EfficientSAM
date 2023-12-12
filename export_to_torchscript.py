995ff765c37715c602d82a9bfcb08d582b8d0ed0
balakv504 <149191765+balakv504@users.noreply.github.com>
2023-12-12 13:30:36 -0800

    Update export_to_torchscript.py

--- export_to_torchscript.py
+++ export_to_torchscript.py
@@ -1,5 +1,5 @@
 import torch
-from efficient_sam.efficient_sam import build_efficient_sam_vitt, build_efficient_sam_vits
+from efficient_sam.build_efficient_sam import build_efficient_sam_vitt, build_efficient_sam_vits
 import zipfile
 
 torch.jit.save(torch.jit.script(build_efficient_sam_vitt()), "torchscripted_model/efficient_sam_vitt_torchscript.pt")
