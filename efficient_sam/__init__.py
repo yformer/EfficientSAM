fc927d1f828a2e89a04e5aa69d5e2041cea81694
balakv504 <149191765+balakv504@users.noreply.github.com>
2023-12-12 13:29:53 -0800

    Update __init__.py

--- efficient_sam/__init__.py
+++ efficient_sam/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
 
-from .efficient_sam import (
+from .build_efficient_sam import (
     build_efficient_sam_vitt,
     build_efficient_sam_vits,
 )
