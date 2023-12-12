4ad80101d5b1f230bfe3a33bf35bcb42c0f62f74
Bala Varadarajan <balakv@fb.com>
2023-12-11 23:39:14 -0800

    - Move model files to efficient_sam folder - Simplify sample_inference.py

--- sample_inference.py
+++ sample_inference.py
@@ -1,27 +1,10 @@
-from efficient_sam import build_efficient_sam
+from efficient_sam.efficient_sam import build_efficient_sam
 from PIL import Image
 from torchvision import transforms
 import torch
 import numpy as np
 
-
-def get_sam_mask_using_points(img_tensor, pts_sampled, model):
-    pts_sampled = torch.reshape(torch.tensor(pts_sampled), [1, 1, -1, 2])
-    max_num_pts = pts_sampled.shape[2]
-    pts_labels = torch.ones(1, 1, max_num_pts)
-    predicted_logits, predicted_iou = model(
-        img_tensor[None, ...],
-        pts_sampled,
-        pts_labels,
-    )
-    # The masks are already sorted by their predicted IOUs.
-    # The first dimension is the batch size (we have a single image. so it is 1).
-    # The second dimension is the number of masks we want to generate (in this case, it is only 1)
-    # The third dimension is the number of candidate masks output by the model.
-    # For this demo we use the first mask.
-    return torch.ge(predicted_logits[0, 0, 0, :, :], 0).cpu().detach().numpy()
-
-
+# Build the model.
 efficient_sam = build_efficient_sam('model_ckpt.pth')
 efficient_sam.eval()
 
@@ -30,9 +13,20 @@ sample_image_np = np.array(Image.open("figs/examples/dogs.jpg"))
 sample_image_tensor = transforms.ToTensor()(sample_image_np)
 # Feed a few (x,y) points in the mask as input.
 
-input_point = np.array([[580, 350], [650, 350]])
-input_label = np.array([1, 1])
+input_points = torch.tensor([[[[580, 350], [650, 350]]]])
+input_labels = torch.tensor([[1, 1]])
 
-mask = get_sam_mask_using_points(sample_image_tensor, input_point, efficient_sam)
+predicted_logits, predicted_iou = efficient_sam(
+    sample_image_tensor[None, ...],
+    input_points,
+    input_labels,
+)
+# The masks are already sorted by their predicted IOUs.
+# The first dimension is the batch size (we have a single image. so it is 1).
+# The second dimension is the number of masks we want to generate (in this case, it is only 1)
+# The third dimension is the number of candidate masks output by the model.
+# For this demo we use the first mask.
+mask = torch.ge(predicted_logits[0, 0, 0, :, :], 0).cpu().detach().numpy()
+
 masked_image_np = sample_image_np.copy().astype(np.uint8) * mask[:,:,None]
 Image.fromarray(masked_image_np).save("figs/examples/dogs_efficient_sam_mask.png")
